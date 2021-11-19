402655d74fa0a779847b59473d136cdd3352db79
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-19 10:45:17 -0800

    ball now bounces off of sides of canvas

--- courses/11_19_21_Tile_Classics/pong-review/game.js
+++ courses/11_19_21_Tile_Classics/pong-review/game.js
@@ -1,14 +1,42 @@
 (function game() {
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext('2d');
+  const fps = 30;
+  let ballX = 0;
+  let ballSpeedX = 5;
+  let ballY = 0;
+  let ballSpeedY = 7;
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  setInterval(updateAll, 1000/fps);
 
+  function updateAll() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
 
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  ctx.arc(100, 100, 10, 0, Math.PI*2, true);
-  ctx.fill();
+    if (ballX > canvas.width) {
+      ballSpeedX *= -1;
+    }
+
+    if (ballX < 0) {
+      ballSpeedX *= -1;
+    }
+
+    if (ballY > canvas.height) {
+      ballSpeedY *= -1;
+    }
+
+    if (ballY < 0) {
+      ballSpeedY *= -1;
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, 10, 0, Math.PI*2, true);
+    ctx.fill();
+  }
+
 
 })();
