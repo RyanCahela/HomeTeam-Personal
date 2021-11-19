0f68aa54442abfeac24e9d3711eff4e560d27720
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-19 10:57:51 -0800

    refactored draw calls

--- courses/11_19_21_Tile_Classics/pong-review/game.js
+++ courses/11_19_21_Tile_Classics/pong-review/game.js
@@ -10,33 +10,73 @@
   setInterval(updateAll, 1000/fps);
 
   function updateAll() {
+    moveAll();
+    drawAll();
+  }
+
+  function moveAll() {
     ballX += ballSpeedX;
     ballY += ballSpeedY;
-
+  
     if (ballX > canvas.width) {
       ballSpeedX *= -1;
     }
-
+  
     if (ballX < 0) {
       ballSpeedX *= -1;
     }
-
+  
     if (ballY > canvas.height) {
       ballSpeedY *= -1;
     }
-
+  
     if (ballY < 0) {
       ballSpeedY *= -1;
     }
-
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-    ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, 10, 0, Math.PI*2, true);
-    ctx.fill();
   }
 
+  function drawAll() {
+    drawRect({
+      topLeftX: 0,
+      topLeftY: 0,
+      boxWidth: canvas.width,
+      boxHeight: canvas.height,
+      fillColor: "black",
+    });
 
+    drawCircle({
+      centerX: ballX,
+      centerY: ballY,
+      radius: 10,
+      arcStart: 0,
+      arcEnd: Math.PI * 2,
+      fillColor: "white"
+    })
+  }
+
+  function drawRect({
+    topLeftX,
+    topLeftY,
+    boxWidth,
+    boxHeight,
+    fillColor
+  }) {
+    ctx.fillStyle = fillColor;
+    ctx.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+  }
+
+  function drawCircle({
+    centerX,
+    centerY,
+    radius,
+    arcStart,
+    arcEnd,
+    isClockwise = true,
+    fillColor,
+  }) {
+    ctx.fillStyle = fillColor;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, arcStart, arcEnd, isClockwise);
+    ctx.fill();
+  }
 })();
