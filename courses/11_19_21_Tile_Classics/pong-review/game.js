3fe3c88c73e2e05e751f198ce68ffd4d42736004
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-19 11:26:31 -0800

    on mousemove collect mouse position, better collision detection for ball against side of canvas

--- courses/11_19_21_Tile_Classics/pong-review/game.js
+++ courses/11_19_21_Tile_Classics/pong-review/game.js
@@ -2,12 +2,24 @@
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext('2d');
   const fps = 30;
-  let ballX = 0;
+  const PADDLE_WIDTH = 1000;
+  const PADDLE_THINCKNESS = 10;
+
+
+  //mouse values
+  let mouseX = 0;
+  let mouseY = 0;
+
+  //ball values
+  let ballX = 100;
   let ballSpeedX = 5;
-  let ballY = 0;
+  let ballY = 100;
   let ballSpeedY = 7;
+  const ballRadius = 10;
 
   setInterval(updateAll, 1000/fps);
+
+  canvas.addEventListener("mousemove", updateMousePosition);
 
   function updateAll() {
     moveAll();
@@ -18,19 +30,19 @@
     ballX += ballSpeedX;
     ballY += ballSpeedY;
   
-    if (ballX > canvas.width) {
+    if (ballX > canvas.width - ballRadius) {
       ballSpeedX *= -1;
     }
   
-    if (ballX < 0) {
+    if (ballX < 0 + ballRadius) {
       ballSpeedX *= -1;
     }
   
-    if (ballY > canvas.height) {
+    if (ballY > canvas.height - ballRadius) {
       ballSpeedY *= -1;
     }
   
-    if (ballY < 0) {
+    if (ballY < 0 + ballRadius) {
       ballSpeedY *= -1;
     }
   }
@@ -47,13 +59,14 @@
     drawCircle({
       centerX: ballX,
       centerY: ballY,
-      radius: 10,
-      arcStart: 0,
-      arcEnd: Math.PI * 2,
+      radius: ballRadius,
       fillColor: "white"
     })
   }
 
+  /**********************
+  HELPER FUNCTIONS 
+  ***********************/
   function drawRect({
     topLeftX,
     topLeftY,
@@ -69,14 +82,23 @@
     centerX,
     centerY,
     radius,
-    arcStart,
-    arcEnd,
-    isClockwise = true,
     fillColor,
   }) {
     ctx.fillStyle = fillColor;
     ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, arcStart, arcEnd, isClockwise);
+    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
     ctx.fill();
+  }
+
+  function updateMousePosition(evt) {
+    const rect = canvas.getBoundingClientRect();
+    const root = document.documentElement;
+
+
+    mouseX = evt.clientX - rect.left - root.scrollLeft;
+    mouseY = evt.clientY - rect.top - root.scrollRight;
+
+    console.log('mouseX', mouseX);
+    console.log('mouseY', mouseY);
   }
 })();
